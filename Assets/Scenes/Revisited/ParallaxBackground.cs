d07bec64b73b7a765698525b68e86f7bb7b4ff5d
ChociMilki <chocimilki@gmail.com>
2024-01-03 20:03:54 -0800

    day cycle parallax scroll in effect

--- Assets/Scenes/Revisited/ParallaxBackground.cs
+++ Assets/Scenes/Revisited/ParallaxBackground.cs
@@ -1,30 +1,47 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
+using System.Runtime.CompilerServices;
+/// <summary>
+/// tt: handles background scroll effect within the "EnvironmentCanvas" 
+///Using this multi canvas 2D UI approach, within Inspector,  I make sure to deactivate box collider (keep active component) so that the player does not run into the collider. 
+/// </summary>
 public class ParallaxBackground : MonoBehaviour
 {
-    [Header("Background Settings")]
-    [SerializeField] private float sjfBackgroundMovementSpeed;
-    private float sjfBackgroundRepeatWidth;
-    private Vector3 sjfBackgroundStartPos;
-    private int half = 2;
+    [Header("Parallax Background Settings")]
+    [SerializeField] private float backgroundMovementSpeed;
+     private Button startStory; 
+    private float colliderRepeatWidth;
+    private Vector3 backgroundOriginPos;
+    private int half = 2; 
+
+   
 
     private void Start()
     {
-        // sets sjfbackground endless scroll 
-        sjfBackgroundStartPos = transform.position;
-        sjfBackgroundRepeatWidth = GetComponent<BoxCollider2D>().size.x / half;
-        Debug.Log("background endless scroll");
+  
+        //set origin position to the transform 
+       backgroundOriginPos = transform.position;
+
+        // grab the box collider and split in half along x for parallax calculation
+       colliderRepeatWidth = GetComponent<BoxCollider2D>().size.x / half;
+     
     }
     void Update()
     {
         CalculateParallax(); 
     }
+
+    /// <summary>
+    ///  tt : move the game object at a certain speed in the -x direction 
+    ///  if  half of the collider has passed then reset to origin position. 
+    /// </summary>
     private void CalculateParallax()
     {
-            transform.Translate(sjfBackgroundMovementSpeed * Time.deltaTime * Vector3.left);
-            if (transform.position.x < sjfBackgroundStartPos.x - sjfBackgroundRepeatWidth)
+            transform.Translate(backgroundMovementSpeed * Time.deltaTime * Vector3.left);
+            if (transform.position.x < backgroundOriginPos.x - colliderRepeatWidth)
             {
-                transform.position = sjfBackgroundStartPos;
+                transform.position = backgroundOriginPos;
             }
         }
 
